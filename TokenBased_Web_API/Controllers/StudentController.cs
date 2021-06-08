2826a447061c61789f132245ab6150208b0a1323
TechnoBoy <DloadJunior@gmail.com>
2021-06-08 06:04:35 +0530

    Added API Controller

---
+++ TokenBased_Web_API/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TokenBased_Web_API.Controllers
+{
+    public class StudentController : ApiController
+    {
+        [Authorize]
+        public IHttpActionResult Get()
+        {
+            using(ClassroomEntities objClass = new ClassroomEntities())
+            {
+                return Ok(objClass.Students.ToList());
+            }
+        }
+    }
+}
